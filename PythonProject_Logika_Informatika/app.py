e2358d3fdae7ef66a87b31ad641804de81a2d391
michaelab <michael.betram123@gmail.com>
2026-01-21 20:37:35 +0700

    M15

--- PythonProject_Logika_Informatika/app.py
+++ PythonProject_Logika_Informatika/app.py
@@ -12,7 +12,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB.Query")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
